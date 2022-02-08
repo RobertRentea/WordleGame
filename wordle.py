efd5c49fb6ce4528997ed241473d9d594284d264
Robert Constantin RENTEA <rrentea@bitdefender.com>
2022-02-08 11:03:25 +0200

    implemented the logic of the game

--- wordle.py
+++ wordle.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import os
 import random
 
@@ -13,6 +14,20 @@ WORDLE_DATA_DIR = os.path.join(
 POSSIBLE_WORDS_FILE = os.path.join(WORDLE_DATA_DIR, 'possible_words.txt')
 ALLOWED_WORDS_FILE = os.path.join(WORDLE_DATA_DIR, 'allowed_words.txt')
 
+
+class Square:
+    def __init__(self, color) -> None:
+        self.color = color
+
+    def __str__(self) -> str:
+        hex_code = self.color.strip('#')
+        red = int(hex_code[:2], 16)
+        green = int(hex_code[2:4], 16)
+        blue = int(hex_code[4:6], 16)
+
+        return f"\033[48:2::{red}:{green}:{blue}m  \033[49m"
+
+
 class WordleGame:
     def __init__(self) -> None:
         with open(POSSIBLE_WORDS_FILE) as possible_words_file:
@@ -21,16 +36,46 @@ class WordleGame:
             self.allowed_words = allowed_words_file.read().split('\n')
 
         self.word = random.choice(self.possible_words)
-        
+        print(self.word)
+        self.guesses = []
+        self.won = False
 
-def square(hex_code: str) -> None:
-    hex_code = hex_code.strip('#')
-    red = int(hex_code[:2], 16)
-    green = int(hex_code[2:4], 16)
-    blue = int(hex_code[4:6], 16)
+    def guess(self, word):
+        print(f'Guess {len(self.guesses) + 1}')
+        if word == self.word:
+            self.won = True
+        self.guesses.append(word)
+        self.draw()
 
-    return f"\033[48:2::{red}:{green}:{blue}m  \033[49m"
+    def finished(self):
+        return len(self.guesses) == 6 or self.won
+
+    def draw(self):
+        for word in self.guesses:
+            line = []
+            for i, letter in enumerate(word):
+                if letter in self.word:
+                    if letter == self.word[i]:
+                        line.append(Square(GREEN))
+                    else:
+                        line.append(Square(YELLOW))
+                else:
+                    line.append(Square(DARK_GRAY))
+            line = [str(square) for square in line]
+            print(' '.join(line))
+
+    def play(self):
+        while not self.finished():
+            word = input()
+            self.guess(word)
+        if self.won:
+            print(f'Congratulation! You have guessed the word in {len(self.guesses)} guess{"es" if len(self.guesses) > 1 else ""}.')
+        else:
+            print(f'Better luck next time.')
+
 
 if __name__ == "__main__":
-    WordleGame()
+    game = WordleGame()
+    game.play()
+
 
